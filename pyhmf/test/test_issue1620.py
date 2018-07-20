13119e2e5e6867600bb0399209abce1602b4de9e
Sebastian Schmitt <sebastian.schmitt@kip.uni-heidelberg.de>
2018-07-20 15:21:33 +0200

    unittestify test of issue1620
    Depends-On: 4247, 4281
    
    Change-Id: I8b8e6bfdb8f277f89e00998091a58fca88c01ffd

--- pyhmf/test/test_issue1620.py
+++ pyhmf/test/test_issue1620.py
@@ -1,20 +1,18 @@
+#!/usr/bin/env python
+import unittest
 import pyhmf as pynn
 
-class GetSpikeCountsTest():
+class GetSpikeCountsTest(unittest.TestCase):
 
-    def __init__(self, max_neurons):
-
+    def test(self, max_neurons=10000):
         for n in range(1,max_neurons):
             self.provoke_crash(n)
 
     def provoke_crash(self, n_neurons):
-        print "{} neuron(s) in population".format(n_neurons)
         pop = pynn.Population(n_neurons, pynn.IF_cond_exp)
         selected = [n_neurons-1]
         pview = pop[selected]
-        print "getting spike counts for neuron(s) {}".format(selected)
         pview.get_spike_counts()
 
 if __name__ == "__main__":
-
-    g = GetSpikeCountsTest(10000)
+    unittest.main()
