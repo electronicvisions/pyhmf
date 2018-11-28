602723382358919341a711e2552f477a6180055e
Paul Meehan <pmeehan@kip.uni-heidelberg.de>
2018-11-28 14:42:02 +0100

    Revert "Adapt to changes in marocco API"
    This reverts commit 87fa678873cadade6e5bdda155c8a369d1969803.
    
    Change-Id: I025a968c79a3203f6cc291c643e63e217416220c

--- pyhmf/test/test_spike_recording.py
+++ pyhmf/test/test_spike_recording.py
@@ -2,7 +2,6 @@
 # -*- coding: utf8 -*-
 import numpy as np
 import pyhmf as pynn
-import pysthal
 import unittest
 
 def pymarocco_available():
@@ -21,7 +20,7 @@ def ESS_available():
         marocco.continue_despite_synapse_loss = True
         marocco.calib_backend = pymarocco.PyMarocco.CalibBackend.Default
         marocco.defects.backend = pymarocco.Defects.Backend.None
-        marocco.hicann_configurator = pysthal.HICANNConfigurator()
+        marocco.hicann_configurator = pymarocco.PyMarocco.HICANNConfigurator
 
         pynn.setup(marocco=marocco)
         pynn.run(1.)
@@ -49,7 +48,7 @@ class TestSpikeRecording(unittest.TestCase):
         marocco.continue_despite_synapse_loss = True
         marocco.calib_backend = pymarocco.PyMarocco.CalibBackend.Default
         marocco.defects.backend = pymarocco.Defects.Backend.None
-        marocco.hicann_configurator = pysthal.HICANNConfigurator()
+        marocco.hicann_configurator = pymarocco.PyMarocco.HICANNConfigurator
 
         setup_params = dict()
         if  pynn.__name__ == "pyhmf":
